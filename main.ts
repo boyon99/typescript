be9efdcce47e3896706bedf52aaae3f233dbb3c6
boyon99 <bo_yon_99@naver.com>
2023-03-06 14:36:13 +0900

    :wrench: conf: Init setting

---
+++ main.ts
@@ -0,0 +1,15 @@
+let isDone: boolean = false;
+let num: number = 4;
+let str: string = "string";
+
+let array1: string[] = ["one", "two"];
+let array2: Array<string> = [];
+
+let array3: number[] = [1,2];
+let array4: Array<number> = [1,2];
+
+let array5: (string | number)[] = [1,2,"s"];
+let array6 : any[] = [1,2,"ss",{}];
+
+let array7: readonly string[] = ["b"];
+
